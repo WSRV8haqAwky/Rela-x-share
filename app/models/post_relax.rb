f880e9221ccd679244bd1c86f64e30bd80330336
WSRV8haqAwky <169635283+WSRV8haqAwky@users.noreply.github.com>
2024-07-08 06:18:23 +0000

    検索機能の追加

--- app/models/post_relax.rb
+++ app/models/post_relax.rb
@@ -9,4 +9,18 @@ class PostRelax < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post_relax = PostRelax.where("caption LIKE?","#{word}")
+    elsif search == "forward_match"
+      @post_relax = PostRelax.where("caption LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post_relax = PostRelax.where("caption LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post_relax = PostRelax.where("caption LIKE?","%#{word}%")
+    else
+      @post_relax = PostRelax.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
+  get "search" => "searches#search"
   resources :users, only: [:index, :show, :edit, :update]
   resources :post_relaxes do
     resource :favorites, only: [:create, :destroy]
