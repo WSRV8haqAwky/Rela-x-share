7f5bc6ecc59cad18a1f48d3c7e080945c7649cfb
WSRV8haqAwky <169635283+WSRV8haqAwky@users.noreply.github.com>
2024-07-30 04:50:01 +0000

    モーダルウィンドウの実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,22 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+$(function(){
+    $(".js-modalWindow").click(function(){
+        $("body").append("<div class='overlay'></div>");
+        $(".overlay").fadeIn(300);
+        var target = $(".card-img-top").attr("src");
+        var largeImage = "<img class='modalContent' src='" + target + "' >";
+        $(".overlay").append(largeImage);
+        $("body").addClass("is-active");
+        return false;
+    });
+
+    $("body").on("click",".overlay", function(){
+        $(this).fadeOut(300, function(){
+            $(this).remove();
+            $("body").removeClass("is-active");
+        });
+    });
+});
